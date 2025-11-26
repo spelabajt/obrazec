84bf1c8176f890cf4043fb492e1eca291431d34e
Špela Bajt <spela.bajt7@gmail.com>
2025-11-26 18:11:59 +0100

    Update required_field.js

--- js/required_field.js
+++ js/required_field.js
@@ -191,7 +191,7 @@ if (loginBtn) {
                     draggable: true
                 }).then((result) => {
                     if (result.isConfirmed) {
-                        window.location.href = 'index.html';
+                        window.location.href = '../index.html';
                     }
                 });
             }
@@ -199,3 +199,4 @@ if (loginBtn) {
     }
 
 });
+
